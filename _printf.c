fecb600b3dc4ced683b8ac25c5a578f0c1820e8c
KobinaOtc <kobinaotchereadjoku@gmail.com>
2021-10-05 14:33:12 +0000

    betty switch and function changes

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * get_func - gets function for conversion
@@ -42,76 +43,53 @@ return(NULL);
 
 int _printf(const char *format, ..)
 {
-int i = 0, sum =0, j, p = 0;
-int (*function)();
+unsigned int i;
+int typeCheck = 0, charRtn = 0;
 
-va_list ap;
-va_start(ap, format);
+va_list arg;
 
-if (!format || (format[0] == '%' && format[1] == '\0'))
+va_start(arg, format);
+
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+_putchar(format[i]);
+charRtn++;
+continue;
+}
+
+if (format[i + 1] == '%')
+
+_putchar('%');
+charRtn++;
+i++;
+continue;
+}
+
+if (format[i + 1] == '\0')
+{
 return (-1);
-
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return(-1);
-
-while (format[i])
-{
-if (format[i] == '%')
-{
-j = check_modifier(format[i + 1]);
-
-if (j == 0)
-{
-if (format[i + 1] != '\0')
-function = get_func(format[i + 1]);
-} else if (j < 0)
-{
-p = 1;
-if (format[i + 1] != '\0')
-function = get_func(format[i + 2]);
-} else
-{
-p = 1;
-sum += j;
-i += j;
-function = get_func(format[i + 1]);
 }
 
-if (function == NULL)
+typeCheck = printTypeChecker(format[i + 1], arg);
+
+if (typeCheck == -1 || typeCheck != 0)
 {
-_putchar(format[i]);
-sum++;
 i++;
-} else
-{
-sum += function(ap);
-i += 2;
-continue;
 }
-} else
+
+if (typeCheck > 0)
 {
-if (p == 0)
-{
-_putchar(format[i]);
-sum++;
-i++;
-} else
-{
-function = get_func(format[i + 1]);
-if (function == NULL)
-{
-_putchar(format[i]);
-sum++;
-i++;
-} else
-{
-sum += function(ap);
-i += 2;
-continue;
+charRtn += typeCheck;
 }
+
+if (typeCheck == 0)
+{
+_putchar('%');
+charRtn++;
 }
-}
-}
-va_end(ap);
-return (sum);
+
+va_end(arg);
+return (charRtn);
 }
